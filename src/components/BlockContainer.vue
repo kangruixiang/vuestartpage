f8982c6266d781eb9b534efc0e9f57e51f1e1840
thisispiggy <thisispiggy@gmail.com>
2019-11-14 14:24:47 -0500

    added white color card

--- src/components/BlockContainer.vue
+++ src/components/BlockContainer.vue
@@ -3,14 +3,14 @@
     <v-container fluid fill-height>
       <v-layout row wrap justify-center align-center>
         <v-card
-          class="ma-8 pa-3"
+          class="ma-8 pa-3 deep-purple lighten-5"
           width="300px"
           v-for="category in categories"
           v-bind:key="category"
         >
           <v-card-title>{{category.name}}</v-card-title>
           <v-divider></v-divider>
-          <v-list>
+          <v-list class="deep-purple lighten-5">
             <v-list-item link v-for="item in category.sites" v-bind:key="item" :href="item.url">
               <v-list-item-content>
                 <v-list-item-title>{{item.name}}</v-list-item-title>
